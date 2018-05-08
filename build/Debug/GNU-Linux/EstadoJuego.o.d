974cf5045ca6ec66b5c06cdb81a79bcc1808e7a6
Unknown <deb8192@gmail.com>
2018-05-08 11:26:48 -0700

    Archivo necesario retocar para funcionamiento

--- build/Debug/GNU-Linux/EstadoJuego.o.d
+++ build/Debug/GNU-Linux/EstadoJuego.o.d
@@ -1,12 +1,38 @@
 build/Debug/GNU-Linux/EstadoJuego.o: EstadoJuego.cpp EstadoJuego.hpp \
  Estado.hpp EstadoPausa.hpp EstadoMenu.hpp EstadoMenuPartidas.hpp \
  EstadoEstadisticas.hpp Juego.hpp EstadosManager.hpp Motor.hpp \
-<<<<<<< HEAD
- EstadoAyuda.hpp Player.hpp Comportamiento.hpp Hud.hpp Nivel.hpp \
+ EstadoAyuda.hpp Player.hpp Comportamiento.hpp Box2D/Box2D/Box2D.h \
+ Box2D/Box2D/Common/b2Settings.h Box2D/Box2D/Common/b2Draw.h \
+ Box2D/Box2D/Common/b2Math.h Box2D/Box2D/Common/b2Timer.h \
+ Box2D/Box2D/Collision/Shapes/b2CircleShape.h \
+ Box2D/Box2D/Collision/Shapes/b2Shape.h \
+ Box2D/Box2D/Common/b2BlockAllocator.h \
+ Box2D/Box2D/Collision/b2Collision.h \
+ Box2D/Box2D/Collision/Shapes/b2EdgeShape.h \
+ Box2D/Box2D/Collision/Shapes/b2ChainShape.h \
+ Box2D/Box2D/Collision/Shapes/b2PolygonShape.h \
+ Box2D/Box2D/Collision/b2BroadPhase.h \
+ Box2D/Box2D/Collision/b2DynamicTree.h \
+ Box2D/Box2D/Common/b2GrowableStack.h Box2D/Box2D/Collision/b2Distance.h \
+ Box2D/Box2D/Collision/b2TimeOfImpact.h Box2D/Box2D/Dynamics/b2Body.h \
+ Box2D/Box2D/Dynamics/b2Fixture.h Box2D/Box2D/Dynamics/b2WorldCallbacks.h \
+ Box2D/Box2D/Dynamics/b2TimeStep.h Box2D/Box2D/Dynamics/b2World.h \
+ Box2D/Box2D/Common/b2StackAllocator.h \
+ Box2D/Box2D/Dynamics/b2ContactManager.h \
+ Box2D/Box2D/Dynamics/Contacts/b2Contact.h \
+ Box2D/Box2D/Dynamics/Joints/b2DistanceJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2Joint.h \
+ Box2D/Box2D/Dynamics/Joints/b2FrictionJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2GearJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2MotorJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2MouseJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2PrismaticJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2PulleyJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2RevoluteJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2RopeJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2WeldJoint.h \
+ Box2D/Box2D/Dynamics/Joints/b2WheelJoint.h Hud.hpp Nivel.hpp \
  tinyxml/tinyxml.h tinyxml/tinystr.h tinyxml/tinystr.h
-=======
- EstadoAyuda.hpp Player.hpp Comportamiento.hpp Hud.hpp
->>>>>>> desarrollo/feature/input
 
 EstadoJuego.hpp:
 
@@ -32,8 +58,81 @@ Player.hpp:
 
 Comportamiento.hpp:
 
+Box2D/Box2D/Box2D.h:
+
+Box2D/Box2D/Common/b2Settings.h:
+
+Box2D/Box2D/Common/b2Draw.h:
+
+Box2D/Box2D/Common/b2Math.h:
+
+Box2D/Box2D/Common/b2Timer.h:
+
+Box2D/Box2D/Collision/Shapes/b2CircleShape.h:
+
+Box2D/Box2D/Collision/Shapes/b2Shape.h:
+
+Box2D/Box2D/Common/b2BlockAllocator.h:
+
+Box2D/Box2D/Collision/b2Collision.h:
+
+Box2D/Box2D/Collision/Shapes/b2EdgeShape.h:
+
+Box2D/Box2D/Collision/Shapes/b2ChainShape.h:
+
+Box2D/Box2D/Collision/Shapes/b2PolygonShape.h:
+
+Box2D/Box2D/Collision/b2BroadPhase.h:
+
+Box2D/Box2D/Collision/b2DynamicTree.h:
+
+Box2D/Box2D/Common/b2GrowableStack.h:
+
+Box2D/Box2D/Collision/b2Distance.h:
+
+Box2D/Box2D/Collision/b2TimeOfImpact.h:
+
+Box2D/Box2D/Dynamics/b2Body.h:
+
+Box2D/Box2D/Dynamics/b2Fixture.h:
+
+Box2D/Box2D/Dynamics/b2WorldCallbacks.h:
+
+Box2D/Box2D/Dynamics/b2TimeStep.h:
+
+Box2D/Box2D/Dynamics/b2World.h:
+
+Box2D/Box2D/Common/b2StackAllocator.h:
+
+Box2D/Box2D/Dynamics/b2ContactManager.h:
+
+Box2D/Box2D/Dynamics/Contacts/b2Contact.h:
+
+Box2D/Box2D/Dynamics/Joints/b2DistanceJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2Joint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2FrictionJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2GearJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2MotorJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2MouseJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2PrismaticJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2PulleyJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2RevoluteJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2RopeJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2WeldJoint.h:
+
+Box2D/Box2D/Dynamics/Joints/b2WheelJoint.h:
+
 Hud.hpp:
-<<<<<<< HEAD
 
 Nivel.hpp:
 
@@ -42,5 +141,3 @@ tinyxml/tinyxml.h:
 tinyxml/tinystr.h:
 
 tinyxml/tinystr.h:
-=======
->>>>>>> desarrollo/feature/input
